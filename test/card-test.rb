4046cf19199428032f79122fba4f480f7becbf8d
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-17 12:07:38 -0600

    complete testing

--- test/card-test.rb
+++ test/card-test.rb
@@ -1,2 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+
+class CardTest < MiniTest::Test
+
+  def test_it_exists
+    card = Card.new(10, "Hearts")
+    assert_instance_of Card, card
+  end
+
+  def test_it_has_a_value
+    card = Card.new(10, "Hearts")
+    assert_equal "10" , card.value
+  end
+
+  def test_it_has_a_suit
+    card = Card.new(10, "Hearts")
+    assert_equal "Hearts" , card.suit
+  end
+
+end

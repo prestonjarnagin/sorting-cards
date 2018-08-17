02580449de804d566535564cebc9a62a663d2a50
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-17 17:57:28 -0600

    Write guess. Still has failures

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,10 +1,29 @@
-
-
+require './lib/card'
 class Guess
+
+  attr_reader :guess,
+              :card
 
   def initialize (guess, card)
     @guess = guess
     @card = card
+    @suit = ""
+    @value = ""
+  end
+
+  def response
+    "#{card.value} of #{card.suit}"
+  end
+
+  def correct?
+    @guess.split(" of ")[1] == @card.suit && @guess.split(" of ")[0] == @card.value
+  end
+
+  def feedback
+    if correct?
+      "Correct"
+    end
+    "Incorrect"
   end
 
 end

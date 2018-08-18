cdb8b9e3eddb353f7e70490e08470b8954d672bd
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-18 13:36:20 -0600

    Round written. Not yet working

--- lib/round.rb
+++ lib/round.rb
@@ -8,23 +8,26 @@ class Round
   def initialize(deck)
     @deck = deck
     @guesses = []
+    @current_card_index = 0
     @current_card = deck[0]
+    @number_correct = 0
   end
 
-  def record_guess(guess)
-    @guesses << guess
-    
-    new_guess = Guess.new(guess_reformat,@current_card)
-    #Take in guess and current card. Pass to guess class
-
-  end
-
-  def feedback
+  def record_guess(guessed_card)
+    new_guess = Guess.new(guessed_card, @current_card)
+    @guesses << new_guess
+    if new_guess.correct?
+      @number_correct += 1
+    end
+    @current_card_index += 1
+    if @deck[@current_card_index] = nil
+      @current_card_index = 0
+    end
 
   end
 
   def percent_correct
-
+    (@number_correct / @guesses.length) * 100
   end
 
 end

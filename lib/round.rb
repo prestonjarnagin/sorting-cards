7960917a93c93d57214f5bf498527fd2ca1c8682
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-19 10:37:07 -0600

    Round and round-test working

--- lib/round.rb
+++ lib/round.rb
@@ -18,20 +18,20 @@ class Round
 
   def record_guess(card)
     guess = Guess.new("#{card[:value]} of #{card[:suit]}", @current_card)
-    binding.pry
     @guesses << guess
     if guess.correct?
-      @number_correct = @number_correct + 1
+      @number_correct += 1
     end
     @current_card_index += 1
     if @current_card_index == deck.cards.count
       @current_card_index = 0
     end
+    @current_card = @deck.cards[@current_card_index]
     guess
   end
 
   def percent_correct
-    (@number_correct/@guesses.count) * 100.0
+    (@number_correct.to_f/@guesses.count) * 100.0
   end
 
 end
--- test/round-test.rb
+++ test/round-test.rb
@@ -47,23 +47,29 @@ class RoundTest < MiniTest::Test
     card_2 = Card.new("4", "Clubs")
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-    round.record_guess({value: "9", suit: "Spades"})
-    assert_equal 0 , round.number_correct
     round.record_guess({value: "3", suit: "Hearts"})
     assert_equal 1 , round.number_correct
     round.record_guess({value: "4", suit: "Clubs"})
     assert_equal 2 , round.number_correct
-    round.record_guess({value: "6", suit: "Spades"})
-    assert_equal 2 , round.number_correct
+    round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 3 , round.number_correct
+    round.record_guess({value: "9", suit: "Hearts"})
+    assert_equal 3 , round.number_correct
+
   end
 
   def test_calculates_percent_correctly
-    skip
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
     round.record_guess({value: "3", suit: "Hearts"})
     assert_equal 100 , round.percent_correct
+    round.record_guess({value: "4", suit: "Clubs"})
+    assert_equal 100 , round.percent_correct
+    round.record_guess({value: "10", suit: "Hearts"})
+    round.record_guess({value: "10", suit: "Hearts"})
+    assert_equal 50 , round.percent_correct
+
   end
 end

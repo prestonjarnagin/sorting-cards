4cde42245eb8f56838bd42bf620ebb1df88c954a
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-21 14:19:13 -0600

    More tests

--- test/card-test.rb
+++ test/card-test.rb
@@ -19,4 +19,11 @@ class CardTest < MiniTest::Test
     assert_equal "Hearts" , card.suit
   end
 
+  def test_it_has_an_actual_value
+    card_1 = Card.new("10", "Hearts")
+    card_2 = Card.new("3", "Spades")
+    assert_equal 103 , card_1.actual_value
+    assert_equal 34 , card_2.actual_value
+  end
+
 end
